bc9a58cc257d0d7fefa444362a82da5f46c78e13
Isaac <isaac.lin.pnc@gmail.com>
2022-12-16 10:07:46 -0500

    commit tree 8cf223002e60e49f6cdb56f604b229d9de258aa2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const main = document.querySelector('main');
+main.remove();
+
+const newHeader = document.createElement("H1");
+document.body.append(newHeader);
+
+newHeader.id = 'victory'
+
+newHeader.textContent('Isaac is the champion')
