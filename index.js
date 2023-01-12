82adac3b56154d2ae2ed779217677370d919b0e9
Isaac <isaac.lin.pnc@gmail.com>
2023-01-12 16:56:29 -0500

    finished lab.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.querySelector('#main');
+main.remove();
+
+const newHeader = document.createElement("H1");
+
+newHeader.id = "victory"
+
+newHeader.textContent = 'Isaac is the champion'
